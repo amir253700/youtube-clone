df8bdd47e46fc7ca46fc01a09fa86dc7bdcc5b43
Amir <kazemi.amir68@gmail.com>
2022-08-17 14:30:25 +0200

    making search a reusable component under UI section

---
+++ src/component/UI/Search.jsx
@@ -0,0 +1,98 @@
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Button,
+  InputBase,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import React from "react";
+
+const SearchContainer = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "50%",
+  border: "1px solid black",
+  display: "flex",
+  flexDirection: "row-reverse",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "50%",
+  },
+  "&:focus > #srch": {
+    display: "flex",
+  },
+  "&:active > #srch": {
+    display: "flex",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 1),
+  pointerEvents: "none",
+  display: "none",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create(["width", "background-color"]),
+    transitionDuration: "0s",
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "100%",
+    },
+  },
+}));
+
+const Search = ({ sx }) => {
+  const styledInputRef = React.useRef();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        ...sx,
+      }}
+    >
+      <SearchContainer
+        tabIndex={0}
+        ref={styledInputRef}
+        onClick={() => {
+          styledInputRef.current.focus();
+        }}
+      >
+        <StyledInputBase placeholder="Search..." />
+        <SearchIconWrapper id="srch">
+          <SearchIcon />
+        </SearchIconWrapper>
+      </SearchContainer>
+      <IconButton
+        sx={{
+          border: "1px solid black",
+          borderRadius: "5px",
+          transform: "translateX(-15px)",
+        }}
+      >
+        <SearchIcon />
+      </IconButton>
+    </Box>
+  );
+};
+
+export default Search;
--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -2,68 +2,20 @@ import {
   AppBar,
   Box,
   Toolbar,
-  Typography,
   Button,
   InputBase,
+  IconButton,
 } from "@mui/material";
-import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import { styled, alpha } from "@mui/material/styles";
 import React from "react";
+import Search from "../UI/Search";
 
-const SearchContainer = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  transition: "all 0.5s ease",
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create(["width", "background-color"]),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "200px",
-      "&:focus": {
-        width: "250px",
-        backgroundColor: alpha(theme.palette.common.white, 0.35),
-      },
-    },
-  },
-}));
-
-const Header = ({ theme }) => {
-  console.log(theme);
+const Header = () => {
   return (
     <Box>
-      <AppBar sx={(theme) => ({ bgcolor: "white", color: "black" })}>
+      <AppBar sx={{ bgcolor: "white", color: "black" }}>
         <Toolbar sx={{ justifyContent: "space-between" }}>
           <IconButton
             size="large"
@@ -74,12 +26,9 @@ const Header = ({ theme }) => {
           >
             <MenuIcon />
           </IconButton>
-          <SearchContainer>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase placeholder="Search" />
-          </SearchContainer>
+          <Box sx={{ flex: "0.7" }}>
+            <Search sx={{ color: "black" }} />
+          </Box>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
